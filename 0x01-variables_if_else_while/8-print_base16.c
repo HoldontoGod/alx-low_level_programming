eaf14f11c9190a11cd58126bb6481aa840a1950b
HoldontoGod <simbeyemonica@gmail.com>
2022-10-16 11:28:12 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
