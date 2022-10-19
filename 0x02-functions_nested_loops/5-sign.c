891db19da0af4a30813006f893bb77029d9b13d5
HoldontoGod <simbeyemonica@gmail.com>
2022-10-18 17:57:07 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,11 +2,12 @@
 
 /**
  * print_sign - Prints the sign of a number
- * @n: The number of which the sign will be printed
+ * @n: The number to be checked for
  * Return: 1 if the number is greater than zero,
  * 0 if the number is zero
  * -1 if the number is less than zero
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
