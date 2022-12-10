c8b040a40ef7d88fe4b906a110a30bcc3a653a04
HoldontoGod <simbeyemonica@gmail.com>
2022-12-10 09:01:54 -0800

    Elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -20,6 +20,7 @@ void close_elf(int elf);
 /**
  * check_elf - Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers
+ *
  * Description: If the file is not an ELF file - exit code 98.
  */
 void check_elf(unsigned char *e_ident)
@@ -42,6 +43,7 @@ void check_elf(unsigned char *e_ident)
 /**
  * print_magic - Prints the magic numbers of an ELF header.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *
  * Description: Magic numbers are separated by spaces.
  */
 void print_magic(unsigned char *e_ident)
@@ -239,6 +241,7 @@ void close_elf(int elf)
  * @argc: The number of arguments supplied to the program
  * @argv: An array of pointers to the arguments.
  * Return: 0 on success.
+ *
  * Description: If the file is not an ELF File or
  *              the function fails - exit code 98.
  */
@@ -268,6 +271,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
 	}
+
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
