1c569d5570ee73e00f16e9730a75f03ee85c2ec7
HoldontoGod <simbeyemonica@gmail.com>
2023-01-12 15:25:16 -0800

    Doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+4-free_dlistint.c
+
+/**
+ * free_dlistint - Frees a linked dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
