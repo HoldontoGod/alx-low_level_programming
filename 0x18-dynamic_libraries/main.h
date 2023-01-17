4568dc7c7a2fdab0c00d54922d8b694eeb223582
HoldontoGod <simbeyemonica@gmail.com>
2023-01-16 18:23:34 -0800

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,10 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int mod(int a, int b);
-extern div_t div(int __numer, int __denom);
 
 #endif /* MAIN_H */
